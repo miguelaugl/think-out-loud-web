baf3bdce6fe60c4829e9290fb68ec6c3215ec107
Miguel <miguelaugl@outlook.com>
2022-02-11 13:28:24 -0300

    feat: add message emitter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,41 @@
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { SocketEvents } from "../../interfaces";
+import { socket } from "../../services";
 
 const CHAT_MAX_LENGTH = 255
 
 export function Header() {
   const [message, setMessage] = useState('')
-  const chatRef = useRef<HTMLTextAreaElement | null>(null)
   const handleMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value)
   }
-  const chatCurrentLength = chatRef.current?.value.length || 0
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    socket.emit(SocketEvents.MESSAGE_SENT, message)
+    setMessage('')
+  }
   return (
     <>
       <h1 className="text-3xl font-bold text-center my-8 uppercase">Think out loud</h1>
-      <div className="w-full max-w-xl bg-gray-700 mx-auto p-4 rounded flex flex-col">
+      <form className="w-full max-w-xl bg-gray-700 mx-auto p-4 rounded flex flex-col" onSubmit={handleSubmit}>
         <h3 className="text-xl font-bold mb-2">Share here your thoughts:</h3>
         <textarea 
           className="w-full rounded-md bg-slate-900 p-4 resize-none"
           maxLength={CHAT_MAX_LENGTH}
-          ref={chatRef}
           value={message}
           onChange={handleMessageChange}
           placeholder="What are you thinking?"
           rows={4}
         />
-        <p className="b-0 text-xs mt-2 ml-auto font-semibold">{chatCurrentLength} / {CHAT_MAX_LENGTH}</p>
-        <button className="bg-gradient-to-r from-blue-400 to-blue-400 hover:to-blue-500 mt-2 font-bold rounded-full px-10 py-2 ml-auto transition duration-300" type="button">Send</button>
-      </div>
+        <p className="b-0 text-xs mt-2 ml-auto font-semibold">{message.length} / {CHAT_MAX_LENGTH}</p>
+        <button 
+          className="bg-gradient-to-r from-blue-400 to-blue-400 hover:to-blue-500 mt-2 font-bold rounded-full px-10 py-2 ml-auto transition duration-300 disabled:opacity-75 disabled:pointer-events-none" 
+          type="submit"
+          disabled={!message.length}
+        >
+          Send
+        </button>
+      </form>
     </>
   );
 }
